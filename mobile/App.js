3cdd8764b6fcd2b52509b34427e0c265fad64541
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-01-12 10:36:18 +0100

    Move App.js to root folder of mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import { StyleSheet, Text, View } from "react-native";
-import AppContainer from "./AppContainer";
+import AppContainer from "./src/AppContainer";
 
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./redux/reducers";
+import rootReducer from "./src/redux/reducers";
 import { Provider } from "react-redux";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
